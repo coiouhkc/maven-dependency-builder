03c7bcbde0ca3db3c90342252b7613436f7a96f5
Alexei Bratuhin <alexei.bratuhin@googlemail.com>
2022-05-13 00:00:17 +0200

    Upgrade to JUnit 5, missing from merge conflict resolution

--- dependency-builder-core/src/test/java/org/abratuhi/mavendepbuilder/parser/PomParserTest.java
+++ dependency-builder-core/src/test/java/org/abratuhi/mavendepbuilder/parser/PomParserTest.java
@@ -1,15 +1,15 @@
 package org.abratuhi.mavendepbuilder.parser;
 
 import org.abratuhi.mavendepbuilder.jaxb.Model;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PomParserTest {
   @Test
-  public void parsePomXml() {
+  void parsePomXml() {
     Model model = PomParser.parsePomXml(new File("src/test/resources/payara-issue-959-deps-0/pom.xml"));
     assertEquals(model.getGroupId(), "org.abratuhi");
     assertEquals(model.getArtifactId(), "payara-issue-959");
